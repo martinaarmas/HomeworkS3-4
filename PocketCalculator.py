45df7688352660634eff0da96e250fb8c4bb5b04
martinaarmas <marti.armas@gmail.com>
2018-11-19 18:19:34 +0100

    Initial commit

--- PocketCalculator.py
+++ PocketCalculator.py
@@ -2,5 +2,17 @@ num1 = int (input("Please enter your first number"))
 operator = input("Please enter your operator")
 num2 = int (input("Please enter your second number"))
 
-if operator == "+"
+if operator == "+":
+    print (num1+num2)
+
+
+if  operator == "-":
+    print (num1-num2)
+
+if operator =="*":
+    print(num1*num2)
+
+if operator =="/":
+    print(num1/num2)
+
 
--- RoundedDiv.py
+++ RoundedDiv.py
@@ -1,4 +1,4 @@
 num1 = int (input("Please enter the first number"))
 num2 = int (input("Enter the number you would like to divide the first number to"))
 
-print ("The rounded value of the division between" ,num1, "and" ,num2, "is" ,int(num1/num2),)
+print ("The rounded value of the division between" ,num1, "and" ,num2, "is" ,int(num1/num2+0.5),)#se pone +0.5 para que se pueda redondear para arriba tambien
